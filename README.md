7b17b285dc755831759d32c5c848a580df87d49c
João Albuquerque <jv.albuquerque@live.com>
2019-06-13 11:35:39 -0300

    MainMenu done

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Template-Game-Jam-2D
-A Simple Template to starte build a game in a Game Jam for Unity 2019.2
+A Simple Template to start build a game in a Game Jam for Unity 2019.2
+
+## Overview
+
+These project can be simply copy and opened in the unity to start a project. This is made to don't lose time with things that isn't the game, like menu, pause, save highscore and things like that.
+
+##DONE
+* Main Menu
+
+##TODO
+* Music set code
+* Save highscore
+* Pause
+* Game Controller
+
+## Authors
+* **João Vitor Filgueira Albuquerque** - [jv-albuquerque](https://github.com/jv-albuquerque)
---
+++ TemplateGameJam2D/Assets/Scripts/MenuController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    //Menus
+    [SerializeField] private GameObject canvasMainMenu = null;
+    [SerializeField] private GameObject canvasSettings = null;
+    [SerializeField] private GameObject canvasCredits = null;
+
+    //Checkers
+    private bool mainMenu;
+    private bool settings;
+    private bool credits;
+
+    void Start()
+    {
+        MainMenu();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (mainMenu)
+                CloseGame();
+            else
+                MainMenu();
+        }
+    }
+
+    /// <summary>
+    /// Function that starts the game
+    /// Used by the Start button
+    /// </summary>
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    /// <summary>
+    /// Fuction that close the game
+    /// Go to desktop
+    /// Used by the Exit Button
+    /// </summary>
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    ///function thats set the canvas of the Settings the visible canvas 
+    /// </summary>
+    public void Settings()
+    {
+        mainMenu = false;
+        credits = false;
+        settings = true;
+
+        canvasMainMenu.SetActive(false);
+        canvasCredits.SetActive(false);
+        canvasSettings.SetActive(true);
+    }
+
+    /// <summary>
+    ///function thats set the canvas of the Credits the visible canvas 
+    /// </summary>
+    public void Credits()
+    {
+        mainMenu = false;
+        credits = true;
+        settings = false;
+
+        canvasMainMenu.SetActive(false);
+        canvasCredits.SetActive(true);
+        canvasSettings.SetActive(false);
+    }
+
+    /// <summary>
+    ///function thats set the canvas of the Main Menu the visible canvas 
+    /// </summary>
+    public void MainMenu()
+    {
+        mainMenu = true;
+        credits = false;
+        settings = false;
+
+        canvasMainMenu.SetActive(true);
+        canvasCredits.SetActive(false);
+        canvasSettings.SetActive(false);
+    }
+}
