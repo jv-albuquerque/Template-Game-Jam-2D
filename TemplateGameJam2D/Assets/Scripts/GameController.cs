38405ee18ffc7fe7df42924cdf6887f88d76c952
João Albuquerque <jv.albuquerque@live.com>
2019-06-14 13:02:22 -0300

    Pause and GameOver screen

---
+++ TemplateGameJam2D/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private PauseController pause;
+
+    private bool paused = false;
+    private bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause = GetComponent<PauseController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && !gameOver)
+        {
+            paused = !paused;
+            pause.Pause();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            gameOver = true;
+            pause.GameOver();
+        }
+
+        if(!pause && !gameOver)
+        {
+            // do everythings here
+        }
+    }
+}
---
+++ TemplateGameJam2D/Assets/Scripts/PauseController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    private bool isPause = false;
+
+    //Menus
+    [SerializeField] private GameObject canvasPause = null;
+    [SerializeField] private GameObject canvasSettings = null;
+    [SerializeField] private GameObject canvasGameOver = null;
+
+    //Checkers
+    private bool pause;
+    private bool settings;
+    private bool gameOver;
+
+    //past settings variables
+    private float timeScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeScale = Time.timeScale;
+
+        canvasPause.SetActive(false);
+        canvasSettings.SetActive(false);
+        canvasGameOver.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        if (gameOver)
+            return;
+
+        //unpause
+        if(isPause)
+        {
+            // if it is in the pause main menu
+            if (pause)
+            {
+                isPause = false;
+                Time.timeScale = timeScale;
+                canvasPause.SetActive(false);
+                canvasSettings.SetActive(false);
+            }
+            // if it is in the settings menu
+            else
+            {
+                MainPause();
+            }
+        }
+        //pause
+        else
+        {
+            pause = true;
+            isPause = true;
+            Time.timeScale = 0;
+            canvasPause.SetActive(true);
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        canvasGameOver.SetActive(true);
+    }
+
+    public void Settings()
+    {
+        pause = false;
+        settings = true;
+        canvasPause.SetActive(false);
+        canvasSettings.SetActive(true);
+    }
+
+    public void MainPause()
+    {
+        pause = true;
+        settings = false;
+        canvasPause.SetActive(true);
+        canvasSettings.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
